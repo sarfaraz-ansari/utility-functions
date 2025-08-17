7f02ad581d554a83b91fd9e4365eb74a534dbc07
Sarfaraj Ansari <sarfarajansari2000@gmail.com>
2025-08-17 15:44:53 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+export const getRandomInt = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+};
+
+export function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+export const kebabCase = (string) => {
+  return string
+    .replace(/([a-z])([A-Z])/g, "$1-$2")
+    .replace(/[\s_]+/g, "-")
+    .toLowerCase();
+};
+
+export const reverseKebabCase = (string) => {
+  return string.replace(/-[a-z\u00E0-\u00F6\u00F8-\u00FE]/g, function (match) {
+    return " " + match.slice(1).toLowerCase();
+  });
+};
